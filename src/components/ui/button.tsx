8b3ed2208ae040e461d04ff3dfa1c4b0f7db8cda
Jee-Yu Yang <jeeyu96@gmail.com>
2024-11-22 12:46:46 +0100

    add google auth

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,6 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        whub: "bg-amber-400 text-white hover:bg-amber-400/80"
       },
       size: {
         default: "h-9 px-4 py-2",
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FcGoogle }  from "react-icons/fc";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 import { Button } from "@/components/ui/button";
 import { 
@@ -22,8 +23,14 @@ interface SignInCardProps {
 
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+    const { signIn} = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const onProviderSignIn = (value: "github" | "google") => {
+        signIn(value);
+    }
 
     return (
         <Card className="w-full h-full p-8">
@@ -61,7 +68,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 <div className="flex flex-col gap-y-2.5">
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => onProviderSignIn("google")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
