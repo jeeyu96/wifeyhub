6f3a2865622f062480ea001c91507f1da2953156
Jee-Yu Yang <jeeyu96@gmail.com>
2024-11-25 11:57:01 +0100

    add avatar sync

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -12,21 +12,36 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../hooks/use-current-user";
+import { Loader, LogOut } from "lucide-react";
 
 export const UserButton = () => {
+    const { data, isLoading } = useCurrentUser();
+
+    if (isLoading) {
+        return <Loader className="size-4 animate-sping text-muted-foreground"/>
+    }
+    if(!data) {
+        return null;
+    }
+
+    const { image, name} = data;
+
+    const avatarFallback = name!.charAt(0).toUpperCase()
+
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
                 <Avatar className="size-10 hover:opacity-75 transition">
-                    <AvatarImage />
+                    <AvatarImage alt={name} src={image} />
                     <AvatarFallback>
-
+                        {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center" side="right" className="w-60">
                 <DropdownMenu>
-                    
+                    <LogOut></LogOut>
                 </DropdownMenu>
             </DropdownMenuContent>
         </DropdownMenu>
