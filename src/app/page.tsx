e2f481da97e732776ca4c075c4096f3a3f5c444d
Jee-Yu Yang <jeeyu96@gmail.com>
2024-11-25 13:44:25 +0100

    add workspace redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/features/auth/components/user-button";
 
-export default function Home () {
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
+
+
+export default function Home () {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceID = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceID) {
+      console.log("Redirect to workspace")
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [workspaceID, isLoading]);
 
   return (
     <div>
