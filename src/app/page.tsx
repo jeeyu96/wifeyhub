73f7312dec0eabe646545e29dd87fb2f18c025e1
Jee-Yu Yang <jeeyu96@gmail.com>
2024-11-26 02:42:42 +0100

    add : redirect to workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,27 @@ import { UserButton } from "@/features/auth/components/user-button";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
 
 
 
 export default function Home () {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
 
   const { data, isLoading } = useGetWorkspaces();
 
-  const workspaceID = useMemo(() => data?.[0]?._id, [data]);
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
 
   useEffect(() => {
     if (isLoading) return;
 
-    if (workspaceID) {
-      console.log("Redirect to workspace")
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`)
     } else if (!open){
       setOpen(true);
     }
-  }, [workspaceID, isLoading, open, setOpen]);
+  }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
     <div>
